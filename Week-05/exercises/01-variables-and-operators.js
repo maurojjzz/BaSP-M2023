9c28606cc51ffe66d28961ac1d1d945aa28b82b3
Mauro Jimenez <maurojim123@gmail.com>
2023-04-15 12:34:47 -0300

    BaSP-W05-2023: Changes in .js structure

--- Week-05/exercises/01-variables-and-operators.js
+++ Week-05/exercises/01-variables-and-operators.js
@@ -1,28 +1,24 @@
 console.log("EXERCISE 1: VARIABLES AND OPERATORS");
 
-//a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+/*a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números
+ en una 3er variable.*/
 console.log('-Excercise 1.a: ');
-
 var nro1=5; 
 var nro2=7;
 var nro3= nro1+nro2;
-
 console.log(nro3);
 
 //b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 console.log('-Excercise 1.b: ');
-
 var str1="Hola ";
 var str2="Mundo";
 var str3= str1 + str2;
-
 console.log(str3);
 
-//c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+/*c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+ guardando el resultado de la suma en una 3er variable (utilizar length).*/
 console.log('-Excercise 1.c: ');
-
 var str1="Radium";
 var str2="Rocket";
 var largo= str1.length + str2.length;
-
 console.log(largo);
