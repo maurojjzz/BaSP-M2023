91c710456d33504a2fc9f4857e748882413ea55c
Mauro Jimenez <maurojim123@gmail.com>
2023-04-23 21:15:39 -0300

    BaSP-W06-2023: More sign-up validations

--- Week-06/js/sign-up.js
+++ Week-06/js/sign-up.js
@@ -9,8 +9,22 @@ var namee=document.getElementById('name');
 var nameError=document.getElementById('name-error');
 var surname=document.getElementById('surname');
 var surnameError=document.getElementById('surname-error');
+var dni=document.getElementById('dni');
+var dniError=document.getElementById('dni-error');
+var birthDay=document.getElementById('birth');
+var birthDayError=document.getElementById('birth-error');
+var phone=document.getElementById('phone');
+var phoneError=document.getElementById('phone-error');
+var postCode=document.getElementById('postal-code');
+var postCodeError=document.getElementById('postal-error');
 
+
+//to set max date in input type date
+birthDay.setAttribute('required',true);
+
+//set 
 repeatPass.disabled=true;
+
 
 email.addEventListener('blur', emailValidation);
 email.addEventListener('focus', emailChanging);
@@ -22,7 +36,14 @@ namee.addEventListener('blur', nameValidation);
 namee.addEventListener('focus', nameChanging);
 surname.addEventListener('blur', surnameValidation);
 surname.addEventListener('focus', surnameChanging);
-
+dni.addEventListener('blur', dniValidation);
+dni.addEventListener('focus', dniChanging);
+birthDay.addEventListener('blur', birthValidation);
+birthDay.addEventListener('focus', birthChanging);
+phone.addEventListener('blur', phoneValidation);
+phone.addEventListener('focus', phoneChanging);
+postCode.addEventListener('blur', postCodeValidation);
+postCodeError.addEventListener('focus', postCodeChanging);
 
 function emailValidation(){
     if (!(emailExpression.test(email.value)) && email.value.length !== 0) {
@@ -33,6 +54,7 @@ function emailValidation(){
        flagToLoginEmail=true;
     }
  }
+ //remove the styles while focus on the input who haven't pass validations
  function emailChanging(){
     emailError.classList.remove('box-error');
     emailError.textContent="";
@@ -64,6 +86,7 @@ function passValidation(){
     }
     return flag;
  }
+ //verify if strings has any number and return true or false
  function hasANumber(password){
     var flag=false;
     for (let i = 0; i < password.length; i++) {
@@ -74,6 +97,7 @@ function passValidation(){
     }
     return flag;
  }
+ //verify if strings has any letter and if it isnt a symbol
  function hasALetter(password){
     var flag=false;
     for (let i = 0; i < password.length; i++){
@@ -170,4 +194,137 @@ function surnameChanging(){
    surnameError.textContent="";
    surname.style.border='1px solid #5a5656';
 }
+function dniValidation(){
+   var flagValidateLetter=false;
+   //to validate if the string has a symbol or a letter
+   if (!hasALetter(dni.value)) {
+         for (let i = 0; i < dni.value.length; i++) {
+            if (isNaN(dni.value[i])) {
+               flagValidateLetter=false;
+               break;
+            }else{
+               flagValidateLetter=true;
+            }
+         } 
+   }else{
+      dniError.textContent='other than numbers are not allowed in this input';
+      dniError.classList.add('box-error');
+      dni.style.border='2px solid red';
+   }
+   if(flagValidateLetter){
+      if(!(dni.value.length >= 7)){
+         dniError.textContent='dni needs to be at least 7 characters long';
+         dniError.classList.add('box-error');
+         dni.style.border='2px solid red';
+      }
+   }else{
+      if (dni.value.length !== 0) {
+         dniError.textContent='other than numbers are not allowed in this input';
+         dniError.classList.add('box-error');
+         dni.style.border='2px solid red';
+      }  
+   }
+}
+function dniChanging(){
+   dniError.classList.remove('box-error');
+   dniError.textContent="";
+   dni.style.border='1px solid #5a5656';
+}
+function birthValidation(){
+   var today= new Date();
+   var inputDate=new Date(birthDay.value);
+   var birthValidated=false;
+   if (birthDay.value === "") {
+      birthDayError.textContent='this cannot be empty';
+      birthDayError.classList.add('box-error');
+      birthDay.style.border='2px solid red';
+   }else{
+      if(!(inputDate < today)){
+         birthDayError.textContent='can`t choose a future date';
+         birthDayError.classList.add('box-error');
+         birthDay.style.border='2px solid red';
+      }else{
+         birthValidated=true;
+      }
+   }
+   return birthValidated;
+}
+function birthChanging(){
+   birthDayError.classList.remove('box-error');
+   birthDayError.textContent="";
+   birthDay.style.border='1px solid #5a5656';
+}
 
+function phoneValidation(){
+   var flagValidateLetter=false;
+   if (!hasALetter(phone.value)) {
+      for (let i = 0; i < phone.value.length; i++) {
+         if (isNaN(phone.value[i])) {
+            flagValidateLetter=false;
+            console.log('tiene simbolo');
+            break;
+         }else{
+            flagValidateLetter=true;
+         }
+      }  
+   }else{
+      phoneError.textContent='other than numbers are not allowed in this input';
+      phoneError.classList.add('box-error');
+      phone.style.border='2px solid red';
+   }
+   if(flagValidateLetter){
+      if(!(phone.value.length === 10)){
+         phoneError.textContent='phone number needs to be 10 characters long';
+         phoneError.classList.add('box-error');
+         phone.style.border='2px solid red';
+      }
+   }else{
+      if (phone.value.length !== 0) {
+         phoneError.textContent='other than numbers are not allowed in this input';
+         phoneError.classList.add('box-error');
+         phone.style.border='2px solid red';
+      }  
+   }
+}
+function phoneChanging(){
+   phoneError.classList.remove('box-error');
+   phoneError.textContent="";
+   phone.style.border='1px solid #5a5656';
+}
+function postCodeValidation(){
+   var flagValidateLetter=false;
+   if (!hasALetter(postCode.value)) {
+      for (let i = 0; i < postCode.value.length; i++) {
+         if (isNaN(postCode.value[i])) {
+            flagValidateLetter=false;
+            console.log('tiene simbolo');
+            break;
+         }else{
+            flagValidateLetter=true;
+         }
+      }  
+   }else{
+      postCodeError.textContent='other than numbers are not allowed in this input';
+      postCodeError.classList.add('box-error');
+      postCode.style.border='2px solid red';
+   }
+   if(flagValidateLetter){
+      if(!(postCode.value.length>=4 && postCode.value.length <=5)){
+         postCodeError.textContent='Postal code needs to be 4 or 5 characters long';
+         postCodeError.classList.add('box-error');
+         postCode.style.border='2px solid red';
+      }
+   }else{
+      if (phone.value.length !== 0) {
+         postCodeError.textContent='other than numbers are not allowed in this input';
+         postCodeError.classList.add('box-error');
+         postCode.style.border='2px solid red';
+      }  
+   }
+}
+
+function postCodeChanging(){
+   postCodeError.classList.remove('box-error');
+   postCodeError.textContent="";
+   postCode.style.border='1px solid #5a5656';
+}
